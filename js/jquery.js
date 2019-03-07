93ff07e63457d6521abcd11ede06134240833c18
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2019-03-07 00:38:55 -0600

    popup does not start displayed
    Alighned popup and got it to start as not displayed

--- js/jquery.js
+++ js/jquery.js
@@ -1,27 +1,31 @@
 // JQuery? More like [REDACTED]
-
 // mouse hover on subscribe
 $(document).ready(function(){
-  $("#submit").hover(function(){
-    $(this).css("background-color", "#A9A9A9");
-  },
-  function(){
-    $(this).css("background-color", "#000000");
-  }); 
+    $("#submit").hover(function(){
+        $(this).css("background-color", "#A9A9A9");
+    },
+                       function(){
+        $(this).css("background-color", "#000000");
+    }); 
 });
 
 // fade out on submit
 $(this).submit(function(){
-  alert("You are now subscribed.");
-  $("#popup").fadeOut("slow");
-  return false;
+    alert("You are now subscribed.");
+    $("#popup").fadeOut("slow");
+    return false;
 });
 
 // fade out on close
 // btw this is a shoutout to yall you motherfuckers who stole my code
 // jk i actually love you. 
 $(document).ready(function(){
-  $("#close").click(function(){
-    $("#popup").fadeOut("fast");
-  });
+    $("#close").click(function(){
+        $("#popup").fadeOut("fast");
+    });
+});
+
+// display popup after 5 seconds
+$(document).ready(function(){
+    $('#popup').delay(2000).fadeIn("fast");
 });
