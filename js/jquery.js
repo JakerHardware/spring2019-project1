17385db963a8c3bcf26d7ffcddb44681607cbac3
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2019-03-06 21:39:38 -0600

    Update jquery.js
    Added functionallity to fade away popup with javascript

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,18 @@
 // JQuery? More like [REDACTED]
+
+// mouse hover on subscribe
 $(document).ready(function(){
   $("#submit").hover(function(){
-    alert("Hovering over submit");
+    $(this).css("background-color", "#A9A9A9");
   },
   function(){
-    alert("No longer hovering over submit");
+    $(this).css("background-color", "#000000");
   }); 
+});
+
+//fade out on submit
+$(this).submit(function(){
+  alert("You are now subscribed.");
+  $("#popup").fadeOut("slow");
+  return false;
 });
