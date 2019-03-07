04c69ac24a2f7a7da13457d147a6802f11167a8a
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2019-03-06 18:03:44 -0600

    ADDED FOUNDATION
    ADded index.html, jquery script, and css/

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+// JQuery? More like [REDACTED]
+$(document).ready(function(){
+  $("#submit").hover(function(){
+    prompt("Hovering over submit");
+  },
+  function(){
+    prompt("No longer hovering over submit");
+  }); 
+});
